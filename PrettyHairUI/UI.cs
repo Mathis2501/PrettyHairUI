c0266559a32403ce2671e9c83f9f16ef5a15e910
Mathias Bellerby Sylvester Nielsen <math130u@edu.eal.dk>
2016-11-15 12:52:22 +0100

    Exit added

--- PrettyHairUI/UI.cs
+++ PrettyHairUI/UI.cs
@@ -10,7 +10,7 @@ namespace PrettyHairUI
 {
     class UI
     {
-        
+
         static void Main(string[] args)
         {
             UI myProgram = new UI();
@@ -19,57 +19,72 @@ namespace PrettyHairUI
 
         public void Run()
         {
-
             while (true)
             {
-                Console.WriteLine("Choose Your Destiny (Create Product)");
+                Console.WriteLine("Choose Your Destiny (Create Product), (Change Price), (EXIT)");
                 string userInput = Console.ReadLine();
-
-                switch (userInput.ToUpper())
-                {
-                    case "CREATE PRODUCT":
-                        Console.WriteLine("Enter the Product Price:");
-                        string Price = Console.ReadLine();
-                        double ProductPrice = TryParse.TryParseDouble(Price);
-
-                        Console.WriteLine("Enter the Product Amount");
-                        string Amount = Console.ReadLine();
-                        double ProductAmount = TryParse.TryParseDouble(Amount);
-
-                        Console.WriteLine("Enter the Product Description");
-                        string Description = Console.ReadLine();
-                        
-                        ProductType Product = new ProductType(ProductPrice, ProductAmount, Description);
-
-                        ProductTypeRepository.Products.Add(Product);
-                        break;
-
-                    case "CHANGE PRICE":
-                        Console.WriteLine("What Product ID do you wish to edit");
-                        
-                        string iD = Console.ReadLine();
-                        int Id = TryParse.TryParseInt(iD);
-                        
-
-
-                        Console.WriteLine("What Should the Product Cost?");
-                        string NewPrice = Console.ReadLine();
-
-                        ProductPrice = TryParse.TryParseDouble(NewPrice);
-                        
-                        Console.WriteLine(ProductTypeRepository.AdjustPrice(ProductTypeRepository.Products[Id-1], ProductPrice));
-
-                        break;
-
-
-                    default:
-                        Console.WriteLine("Wrong Input!");
-                        break;
-                }
+                RunSwitch(userInput);
             }
         }
-            
 
+        static void RunSwitch(string userInput)
+        {
+            switch (userInput.ToUpper())
+            {
+                case "CREATE PRODUCT":
+                    UI CreateProduct = new UI();
+                    CreateProduct.CreateProduct();
+                    break;
+
+                case "CHANGE PRICE":
+                    UI ChangePrice = new UI();
+                    ChangePrice.ChangePrice();
+                    break;
+
+                case "EXIT":
+                    Environment.Exit(0);
+                    break;
+
+                default:
+                    Console.WriteLine("Wrong Input!");
+                    break;
+            }
         }
+
+        void CreateProduct()
+        {
+            Console.WriteLine("Enter the Product Price:");
+            string Price = Console.ReadLine();
+            double ProductPrice = TryParse.TryParseDouble(Price);
+
+            Console.WriteLine("Enter the Product Amount");
+            string Amount = Console.ReadLine();
+            double ProductAmount = TryParse.TryParseDouble(Amount);
+
+            Console.WriteLine("Enter the Product Description");
+            string Description = Console.ReadLine();
+
+            ProductType Product = new ProductType(ProductPrice, ProductAmount, Description);
+
+            ProductTypeRepository.Products.Add(Product);
+        }
+
+        void ChangePrice()
+        {
+            Console.WriteLine("What Product ID do you wish to edit");
+            string iD = Console.ReadLine();
+            int Id = TryParse.TryParseInt(iD);
+
+            Console.WriteLine("What Should the Product Cost?");
+            string NewPrice = Console.ReadLine();
+            double ProductPrice = TryParse.TryParseDouble(NewPrice);
+
+            Console.WriteLine(ProductTypeRepository.AdjustPrice(ProductTypeRepository.Products[Id - 1], ProductPrice));
+        }
+
+        void Change
     }
+}
+    
+    
 
